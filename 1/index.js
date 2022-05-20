c6c317c8164006a72a6df6d43aa3e5e071b8a9e6
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2022-05-20 12:05:07 +0200

    ref:task 1 and 2

--- 1/index.js
+++ 1/index.js
@@ -1,12 +1,12 @@
 import { first } from "../data.js";
 import { namesList, reducedScore } from "../helpers.js";
-import { join, map } from "ramda";
+import { join, map, zipObj } from "ramda";
 
 const reduceElement = (element) => {
-  return {
-    totalScore: reducedScore(element),
-    names: `Team:  ${join(", ", namesList(element))}`,
-  };
+  return zipObj(
+    ["totalScore", "names"],
+    [reducedScore(element), `Team:  ${join(", ", namesList(element))}`]
+  );
 };
 
 const solve = (...arr) => map((el) => reduceElement(el), arr);
